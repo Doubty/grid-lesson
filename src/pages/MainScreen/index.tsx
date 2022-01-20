6b11c9cbbe457f62f9bc871de86d44749928ae49
doubty <goantonio80@gmail.com>
2022-01-19 22:54:11 -0300

    feat: adicionado o content da aplicação

--- src/pages/MainScreen/index.tsx
+++ src/pages/MainScreen/index.tsx
@@ -36,7 +36,70 @@ function MainScreen() {
                 </ul>
             </nav>
 
-           
+            <main className="content">
+
+                <div className="titulo">
+                    <h1>Harry Potter</h1>
+                    <span> A criança que sobreviveu a Voldermort</span>
+                </div>
+
+                <div className="caracteristicas">
+                    <div>
+                        <span className="numero">
+                            72
+                        </span>
+                        <span className="rotulo">
+                            Fogo
+                        </span>
+                    </div>
+                    <div>
+                        <span className="numero">
+                            72
+                        </span>
+                        <span className="rotulo">
+                            Raio
+                        </span>
+                    </div>
+                </div>
+
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero harum doloribus eveniet a quam perferendis alias consequatur unde, voluptas qui adipisci assumenda eius doloremque est quas accusantium sequi ipsum ab!</p>
+
+                <img src={brasao} alt="brasao" />
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates corrupti, accusamus eius deserunt laudantium magnam aliquam, earum praesentium est ab aut numquam architecto amet, ut fuga porro quo eaque nam.</p>
+                <img src={sonserina} alt="brasao" />
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores nemo quisquam quo alias tempora assumenda repellendus. Aspernatur quod et ea, libero eligendi quidem, aut possimus saepe quis nisi dolore iusto?
+                </p>
+
+
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis numquam possimus suscipit quisquam quasi temporibus incidunt quibusdam. Repudiandae sint dicta nesciunt accusamus accusantium facere doloremque, asperiores commodi rem alias? Fugiat?</p>
+
+                <blockquote>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt pariatur illo obcaecati, ipsam perspiciatis tenetur repellat quidem earum odit ratione blanditiis ullam, iure dicta porro eaque aliquam aspernatur facere sed.</p>
+                    <cite> Harrpy potter books</cite>
+                </blockquote>
+
+                <ul className="atributos">
+                    <li>Tem o poder de fogo</li>
+                    <li>Tem o poder de fogo</li>
+                    <li>Tem o poder de fogo</li>
+                    <li>Tem o poder de fogo</li>
+                    <li>Tem o poder de fogo</li>
+                    <li>Tem o poder de fogo</li>
+                </ul>
+
+                <div className="informacoes">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti ipsam numquam laborum assumenda, repellendus reprehenderit minima, sunt cumque velit consequatur, neque voluptatibus. Illum enim placeat qui culpa saepe, natus sit.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus velit repellat beatae dicta, temporibus aut accusamus illum at nisi, impedit porro placeat! Iste, molestias natus debitis praesentium commodi quibusdam harum.</p>
+
+                </div>
+
+                <img src={castelo} alt="castelo" />
+            </main>
+
+
+     
 
         </div>
     );
